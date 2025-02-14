14a7586fc86d0065ae8838de8881bc218b14722f
Ganesh Hubale <ganeshhubale03@gmail.com>
2025-02-14 12:11:11 +0530

    Update Q5-maxSubarrySum.py

--- python/step2/Q5-maxSubarrySum.py
+++ python/step2/Q5-maxSubarrySum.py
@@ -1,35 +1,22 @@
 # Maximum subarry
 # Given an integer array nums, find the contiguous subarray (containing at 
 # least one number) which has the largest sum, and return the sum.
-import sys
 def max_subarray(nums):
-
-    # current_sum = nums[0]
-    # max_sum = nums[0]
-    n = len(nums)
-
-    # for i in range(1, n):
-
-    #     current_sum =  max(nums[i], current_sum + nums[i])
-    #     max_sum = max(max_sum, current_sum)
+    # Initialize variables
+    max_current = max_global = nums[0]
     
-    # return max_sum
-
-    ###
-    sum = 0
-    max_sum = -sys.maxsize-1
-
-    for i in range(len(nums)):
-        sum = sum + nums[i]
-        if sum > max_sum:
-            max_sum = sum
-        
-        if sum < 0:
-            sum = 0
-    return max_sum
-
-nums = [0, 1, 2, 3, 4, -5, 11]
-print(max_subarray(nums=nums))
+    # Iterate through the array starting from the second element
+    for num in nums[1:]:
+        # Update max_current
+        max_current = max(num, max_current + num)
+        # Update max_global
+        max_global = max(max_global, max_current)
+    
+    return max_global
+    
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+result = max_subarray(nums)
+print(result)  # Output: 6
 
 # Time complexity : O(n)
 # Space complexity : O(1)
